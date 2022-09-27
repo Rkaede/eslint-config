107ac5dbf924cbc63e03f43704a76a5b07eda631
coreym <corey.mac@gmail.com>
2022-09-27 20:28:13 +1000

    WIP

--- index.js
+++ index.js
@@ -7,16 +7,13 @@ module.exports = {
   env: {
     browser: true,
   },
-  plugins: ['@typescript-eslint'],
   extends: [
     'eslint:recommended',
     'plugin:jsx-a11y/strict',
     'plugin:react/recommended',
     'plugin:prettier/recommended',
     'plugin:import/recommended',
-    'plugin:import/typescript',
   ],
-  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 'latest',
   },
@@ -28,4 +25,41 @@ module.exports = {
     ],
     'import/first': 'error',
   },
+  overrides: [
+    {
+      files: ['**/*.{ts,tsx}'],
+      settings: {
+        'import/parsers': {
+          '@typescript-eslint/parser': ['.ts', '.tsx'],
+        },
+        'import/resolver': {
+          typescript: {
+            project: './tsconfig.json',
+          },
+        },
+      },
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        tsconfigRootDir: __dirname,
+        project: './tsconfig.json',
+      },
+      plugins: ['@typescript-eslint'],
+      extends: [
+        'eslint:recommended',
+        'plugin:jsx-a11y/strict',
+        'plugin:react/recommended',
+        'plugin:prettier/recommended',
+        'plugin:import/recommended',
+        'plugin:import/typescript',
+      ],
+      rules: {
+        'react/react-in-jsx-scope': 'off',
+        'prettier/prettier': [
+          'error',
+          { singleQuote: true, endOfLine: 'auto', jsxSingleQuote: false },
+        ],
+        'import/first': 'error',
+      },
+    },
+  ],
 };
